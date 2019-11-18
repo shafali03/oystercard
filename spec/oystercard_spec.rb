d4e8194e67e9722105c27bfd7c33a53b657f402b
Hyan18 <Hyan18@github.com>
2019-11-18 14:58:19 +0000

    can top up oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,26 @@
 require 'oystercard'
 
 describe Oystercard do
+
   subject(:oystercard) { described_class.new }
+
   it 'should start with a balance of zero' do
    expect(oystercard.balance).to eq(0)
   end
+
+  describe "#top_up" do
+
+    it "should add £1 to the oystercard" do
+      oystercard.top_up(1)
+      expect(oystercard.balance).to eq(1)
+    end
+
+    it "should add £1 then £2 to the oystercard" do
+      oystercard.top_up(1)
+      oystercard.top_up(2)
+      expect(oystercard.balance).to eq(3)
+    end
+
+  end
+
 end

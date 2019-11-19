1b1070f25671ca52fec56bd19be1102bedde46bf
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2019-11-19 17:56:52 +0000

    saved journey as a hash key inside an array

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,6 @@
 class Oystercard
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :exit_station, :journey
 
   MAX_BALANCE = 90
   MIN_FARE = 1
@@ -8,6 +8,8 @@ class Oystercard
   def initialize
     @balance = 0
     @entry_station = nil
+    @exit_station = nil
+    @journey = []
   end
 
   def top_up(amount)
@@ -20,17 +22,21 @@ class Oystercard
     @entry_station != nil
   end
 
-  def touch_in(station)
+  def touch_in(entry_station)
     raise "Card already touched in" if in_journey?
 
     raise "Insufficient balance, cannot touch in: please top up" unless balance >= MIN_FARE
-    @entry_station = station
+    @entry_station = entry_station
+    in_journey?
   end
 
-  def touch_out
+  def touch_out(exit_station)
     raise "Card not touched in" unless in_journey?
     deduct(MIN_FARE)
+    @exit_station = exit_station
+    add_complete_journey(@entry_station, @exit_station)
     @entry_station = nil
+    in_journey?
   end
 
   private
@@ -38,6 +44,10 @@ class Oystercard
   def deduct(amount)
 
     @balance -= amount
+  end
+
+  def add_complete_journey(entry_station, exit_station)
+    @journey << {entry_station: entry_station, exit_station: exit_station}
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,9 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:oystercard) { described_class.new }
-  let(:station) {double :station}
+  let(:entry_station) {double :station}
+  let(:ex_station) {double :station}
+  let(:journey) { {entry_station: entry_station, exit_station: ex_station} }
 
   it 'should start with a balance of zero' do
     expect(oystercard.balance).to eq(0)
@@ -27,7 +29,7 @@ describe Oystercard do
 
     it "should touch out" do
       oystercard.touch_in(:station)
-      oystercard.touch_out
+      oystercard.touch_out(:ex_station)
       expect(oystercard.in_journey?).to eq false
     end
 
@@ -66,24 +68,40 @@ describe Oystercard do
     it "should raise an error when balance is insufficient" do
       expect { oystercard.touch_in(:station) }.to raise_error "Insufficient balance, cannot touch in: please top up"
     end
-    
+
     it "save entry station at touch in" do
       oystercard.top_up(10)
-      oystercard.touch_in(station)
-      expect(oystercard.entry_station).to eq(station)
+      oystercard.touch_in(entry_station)
+      expect(oystercard.entry_station).to eq(entry_station)
   end
 
 end
 
   describe "#touch_out" do
     it "should not allow you to touch out if not on a journey" do
-      expect { oystercard.touch_out }.to raise_error "Card not touched in"
+      expect { oystercard.touch_out(:ex_station) }.to raise_error "Card not touched in"
     end
 
     it "deduct minimum fare from balance when touching out" do
       oystercard.top_up(10)
       oystercard.touch_in(:station)
-      expect { oystercard.touch_out }.to change{oystercard.balance}.by(-Oystercard::MIN_FARE)
+      expect { oystercard.touch_out(:ex_station) }.to change{oystercard.balance}.by(-Oystercard::MIN_FARE)
+    end
+
+    it "accepts a argument when touch out method" do
+      oystercard.top_up(10)
+      oystercard.touch_in("Makers")
+      oystercard.touch_out("shoreditch")
+      expect( oystercard.exit_station).to eq("shoreditch")
+    end
+  end
+
+  describe "#show_journey" do
+    it "displays the previus journey after completing 1 full journey" do
+      oystercard.top_up(10)
+      oystercard.touch_in(entry_station)
+      oystercard.touch_out(ex_station)
+      expect(oystercard.journey).to include(journey)
     end
   end
 end

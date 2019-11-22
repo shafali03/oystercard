8a425f064648e061957c0833275511546fabd903
shafali03 <shaf03sm@gmail.com>
2019-11-22 16:26:31 +0000

    Added finish and fare metthod

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,8 +1,12 @@
 
 class Journey
 
+
   attr_reader :entry_station, :entry_zone, :exit_station, :exit_zone
   
+  MIN_FARE = 1
+  PENALTY = 6 
+
  def start(station)
    @entry_station = station.name
    @entry_zone = station.zone
@@ -12,5 +16,26 @@ class Journey
    @exit_station = station.name
    @exit_zone = station.zone
  end
+
+
+ def fare
+  if @entry_station == nil || @exit_station == nil
+  PENALTY
+  else
+    MIN_FARE
+
+ end
+end
+
+
+
+
+
+
+
+
+
+
+
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -7,22 +7,59 @@ let(:station) {double :station, name: 'Liverpool Station', zone: 1}
 let(:station_2) {double :station_2, name: 'Aldgate Station', zone: 2}
 
   describe '#start'do 
+  before do
+    subject.start(station)
+  end
     it 'should have a entry station' do
-      subject.start(station)
         expect(subject.entry_station).to eq('Liverpool Station')
       end
     
 
     it 'should have a entry zone' do 
-      subject.start(station)
         expect(subject.entry_zone).to eq(1)
       end
     end
 
+
   describe '#finish' do
+  before do
+    subject.finish(station_2)
+  end
     it 'should have a finish exit station' do
-      subject.finish(station_2)
         expect(subject.exit_station).to eq('Aldgate Station')
       end
+    
+
+    it 'should ahve a finish exit zone' do
+        expect(subject.exit_zone).to eq(2)
     end
+  end
+
+  describe '#fare' do  
+  before do
+    subject.start(station)
+
+  end
+    it 'should return minimum fare if the journey is complete' do
+      subject.finish(station_2)
+      expect(subject.fare).to eq(Journey::MIN_FARE)
+    end
+
+    it 'should return penalty if start journey is not complete' do 
+      expect(subject.fare).to eq(Journey::PENALTY)
+    end
+  end
+
+
+
+
+
+
+
+
+
 end
+
+
+
+
